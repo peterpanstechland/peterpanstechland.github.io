6e4c3d66a7d476ea2963aa2b5c3cc88206ee352c
Peter Pan <twinkle-pirate@hotmail.com>
2024-07-16 11:07:50 +0800

    Starting New

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/peter-smg/Documents/selfproject/peterpanstechland.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/peter-smg/Documents/selfproject/peterpanstechland.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/peter-smg/Documents/selfproject/peterpanstechland.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/peter-smg/Documents/selfproject/peterpanstechland.github.io/src/css/custom.css"),
+];
